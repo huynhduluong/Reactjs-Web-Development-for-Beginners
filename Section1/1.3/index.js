945894f911e0f4b1fef433eac4a4538821c6a331
huynhduluong <woverine147@gmail.com>
2019-10-09 05:02:42 +0700

    add 1.3

---
+++ Section1/1.3/index.js
@@ -0,0 +1,19 @@
+// if (true) {
+//     var name ='Ryan';
+//     // alert(name);
+// }
+
+// alert(name);
+
+// if (true) {
+//     let name ='Ryan';
+//     // alert(name);
+// }
+
+// alert(name);
+
+if (true) {
+    let name ='Ryan';
+    name = 'Zen';
+    alert(name);
+}
