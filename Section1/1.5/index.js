c1781e2c22e468b162ea756a2cf9059250983ad7
huynhduluong <woverine147@gmail.com>
2019-10-09 05:13:47 +0700

    add 1.5

---
+++ Section1/1.5/index.js
@@ -0,0 +1,11 @@
+// function welcome(user, message) {
+//     alert(`Hello ${user}, ${message}`);
+// }
+
+// welcome("Ryan", "Good morning");
+
+function welcome(user="Ryan", message="Good morning") {
+    alert(`Hello ${user}, ${message}`);
+}
+
+welcome();
