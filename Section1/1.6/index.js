0c1aab5d0e12cc9d02fce30059daeca4fc18cc15
huynhduluong <woverine147@gmail.com>
2019-10-09 05:24:07 +0700

    add 1.6

---
+++ Section1/1.6/index.js
@@ -0,0 +1,22 @@
+// function greeting(message) {
+//     return alert(`${message} everyone!`);
+// }
+
+
+let greeting = () => alert(`Hello everyone!`);
+
+greeting();
+
+// let createBlog = (title, body) => {
+//     if (!title) {
+//         throw new Error('A title is required');
+//     }
+
+//     if (!body) {
+//         throw new Error('Body cant be empty');
+//     }
+
+//     return alert(`${title} - ${body}`);
+// }
+
+// createBlog('Blog title', 'Blog body');
