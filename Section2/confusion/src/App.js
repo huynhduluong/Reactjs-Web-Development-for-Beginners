9310b8c952017abaaa3a1f358de27114d6cc3b54
huynhduluong <woverine147@gmail.com>
2019-10-09 19:52:23 +0700

    rendering state data using map

--- Section2/confusion/src/App.js
+++ Section2/confusion/src/App.js
@@ -9,19 +9,27 @@ class App extends Component {
     }
   };
 
-  
-  componentWillMount() {
+  getUsers() {
     axios('https://api.randomuser.me/?nat=US&results=5').then(response => 
       this.setState({
         users: response.data.results
       })
     );
   }
+
+  
+  componentWillMount() {
+    this.getUsers();
+  }
   
   render() {
     return (
-      <div className="App">
-        we will be back
+      <div className="App">{this.state.users.map(user => 
+        <div>
+          <h3>{user.name.first}</h3>
+          <p>{user.email} </p>
+          <hr />
+        </div>)}
       </div>
     );
   }
