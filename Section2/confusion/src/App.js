50682c7e58de3a3123a4551875bc584f6d4659a9
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-14 21:02:51 +0700

    finish section 2

--- Section2/confusion/src/App.js
+++ Section2/confusion/src/App.js
@@ -1,35 +1,57 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Loading from './Loading';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
-    }
+      users: [],
+      loading: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
 
   getUsers() {
+    this.setState({
+      loading: true
+    })
     axios('https://api.randomuser.me/?nat=US&results=5').then(response => 
       this.setState({
-        users: response.data.results
+        users: [...this.state.users,  ...response.data.results],
+        loading: false
       })
     );
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getUsers();
+    console.log('more users loaded');
+    
+  }
   
   componentWillMount() {
     this.getUsers();
   }
   
   render() {
+    const {loading, users} = this.state;
     return (
-      <div className="App">{this.state.users.map(user => 
-        <div>
-          <h3>{user.name.first}</h3>
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+            <input type="submit" value="load users" />
+        </form>
+      {!loading 
+      ? this.state.users.map(user => (
+        <div key={user.id.value}>
+          <h3 style={{ color: 'red' }}>{user.name.first}</h3>
           <p>{user.email} </p>
           <hr />
-        </div>)}
+          
+       </div>
+       ))
+      : (<Loading message="Hey Hey Hey" />)}
       </div>
     );
   }
