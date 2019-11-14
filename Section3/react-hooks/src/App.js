0960a762f52c9a27c2d53fa4ebe5c7e8f75a7620
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-14 21:59:41 +0700

    useState hook

--- Section3/react-hooks/src/App.js
+++ Section3/react-hooks/src/App.js
@@ -1,24 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-class App extends Component {
-  state = {
-    count: 0
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
   };
-
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <h2>counter app</h2>
-        <button onClick={this.increment}>Clicked {this.state.count} times </button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>counter app</h2>
+      <button onClick={increment}>Clicked {count} times </button>
+    </div>
+  );
 }
+
+// class App extends Component {
+//   state = {
+//     count: 0
+//   };
+
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>counter app</h2>
+//         <button onClick={this.increment}>Clicked {this.state.count} times </button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
