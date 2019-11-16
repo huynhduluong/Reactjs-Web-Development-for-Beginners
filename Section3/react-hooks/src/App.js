8414a11524fd43cabd711d3852ddd272ccf4c428
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-16 22:34:42 +0700

    Loading

--- Section3/react-hooks/src/App.js
+++ Section3/react-hooks/src/App.js
@@ -5,13 +5,16 @@ const App = () => {
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react');
   const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
+  const [loading, setLoading] = useState(false);
   //fetch news
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    //set loading true
+    setLoading(true);
+    fetch(url)
     .then(result => result.json())
     // .then(data => console.log(data));
     
-    .then(data => setNews(data.hits))
+    .then(data => (setNews(data.hits), setLoading(false)))
     .catch(error => console.log(error));
   };
   useEffect(() => {
@@ -28,6 +31,7 @@ const App = () => {
   return(
     <div>
       <h2>News</h2>
+      {loading ? <h2>Loading...</h2> : ""}
       <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange} />
         <button>Search</button>
