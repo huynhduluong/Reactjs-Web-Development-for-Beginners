2f9df766d0db9797d23a2b904a09116ec3c30dd0
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-16 22:16:34 +0700

    controlling useEffect's behavior

--- Section3/react-hooks/src/App.js
+++ Section3/react-hooks/src/App.js
@@ -4,6 +4,7 @@ const App = () => {
   //state
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
   //fetch news
   const fetchNews = () => {
     fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
@@ -15,15 +16,19 @@ const App = () => {
   };
   useEffect(() => {
     fetchNews();
-  });
+  }, [url]);
   const handleChange = e => {
     setSearchQuery(e.target.value)
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
   }
 
   return(
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange} />
         <button>Search</button>
       </form>
